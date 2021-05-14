76fb7e645088238525f443d5e00dc1a5278825fb
Alex Grobovsky <ktotaam@mail.ru>
2021-05-14 10:11:06 +0500

    feat: add formula tooltips slider

---
+++ insein-relax/layout/dist/main.b6585ce837f4283a1e35.hot-update.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_3dglo"]("main",{
+
+/***/ "./src/modules/tooltipHandler.js":
+/*!***************************************!*\
+  !*** ./src/modules/tooltipHandler.js ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _carousel__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./carousel */ \"./src/modules/carousel.js\");\n\n\nvar tooltipHandler = function tooltipHandler() {\n  var formulaSection = document.getElementById('formula');\n  formulaSection.addEventListener('mouseover', function (e) {\n    var target = e.target;\n\n    if (target.closest('.formula-item')) {\n      target = target.closest('.formula-item');\n      var itemHeight = target.clientHeight;\n      var tooltip = target.querySelector('.formula-item-popup'); // console.log(tooltip.getBoundingClientRect().top)\n\n      var tooltipHeight = tooltip.clientHeight; // const tooltipBottom = window.innerHeight - tooltip.getBoundingClientRect().top - tooltip.offsetHeight;\n      // const distanceBetween = tooltipBottom - target.getBoundingClientRect().top;\n\n      var distance = itemHeight + tooltipHeight + 20;\n      var row = target.closest('.row');\n\n      if (target.getBoundingClientRect().top < tooltipHeight + 20) {\n        target.classList.add('js-inversed');\n        row.classList.add('js-index');\n        tooltip.style.transform = \"translateY(\".concat(distance, \"px)\");\n      } else {\n        target.classList.remove('js-inversed');\n        tooltip.style.transform = \"translateY(0)\";\n      }\n\n      target.classList.add('active-item');\n    }\n  });\n  formulaSection.addEventListener('mouseout', function (e) {\n    var target = e.target;\n\n    if (target.closest('.formula-item')) {\n      target = target.closest('.formula-item');\n      var row = target.closest('.row');\n      row.classList.remove('js-index');\n      target.classList.remove('active-item');\n    }\n  });\n\n  if (document.documentElement.clientWidth > 1024) {\n    return;\n  }\n\n  var formulaSlider = new _carousel__WEBPACK_IMPORTED_MODULE_0__.default({});\n};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (tooltipHandler);\n\n//# sourceURL=webpack://3dglo/./src/modules/tooltipHandler.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("fb52883bec5612528bea")
+/******/ })();
+/******/ 
+/******/ }
+);
--- insein-relax/layout/src/modules/tooltipHandler.js
+++ insein-relax/layout/src/modules/tooltipHandler.js
@@ -1,45 +1,71 @@
-//TODO z index
+//TODO ЧТО С ТУЛТИПАМИ В СЛАЙДЕРЕ? Как и когда они показываются
+
+import Carousel from './carousel';
 
 const tooltipHandler = () => {
   const formulaSection = document.getElementById('formula');
 
-  formulaSection.addEventListener('mouseover', (e) => {
-    let target = e.target;
+  if (document.documentElement.clientWidth > 1024) {
+    formulaSection.addEventListener('mouseover', (e) => {
+      let target = e.target;
 
+      if (target.closest('.formula-item')) {
+        target = target.closest('.formula-item');
+        const itemHeight = target.clientHeight;
+        const tooltip = target.querySelector('.formula-item-popup');
+        // console.log(tooltip.getBoundingClientRect().top)
+        const tooltipHeight = tooltip.clientHeight;
+        // const tooltipBottom = window.innerHeight - tooltip.getBoundingClientRect().top - tooltip.offsetHeight;
+        // const distanceBetween = tooltipBottom - target.getBoundingClientRect().top;
+        const distance = itemHeight + tooltipHeight + 20;
+        const row = target.closest('.row');
+
+
+        if (target.getBoundingClientRect().top < tooltipHeight + 20) {
+          target.classList.add('js-inversed');
+          row.classList.add('js-index');
+          tooltip.style.transform = `translateY(${distance}px)`;
+        } else {
+          target.classList.remove('js-inversed');
+          tooltip.style.transform = `translateY(0)`;
+        }
+
+        target.classList.add('active-item');
+      }
+    });
+
+    formulaSection.addEventListener('mouseout', (e) => {
+      let target = e.target;
+
+      if (target.closest('.formula-item')) {
+        target = target.closest('.formula-item');
+        const row = target.closest('.row');
+        row.classList.remove('js-index');
+        target.classList.remove('active-item');
+      }
+    });
+  }
+
+  const formulaSlider = new Carousel({
+    main: '.formula-slider-wrap',
+    wrapper: '.formula-slider',
+    next: '#formula-arrow_right',
+    prev: '#formula-arrow_left',
+    slidesToShow: 3,
+  });
+
+  formulaSlider.init();
+  const formulaItems = document.querySelectorAll('.formula-item');
+
+  formulaSection.addEventListener('click', (e) => {
+    let target = e.target;
     if (target.closest('.formula-item')) {
       target = target.closest('.formula-item');
-      const itemHeight = target.clientHeight;
-      console.log(itemHeight)
-      const tooltip = target.querySelector('.formula-item-popup');
-      // console.log(tooltip.getBoundingClientRect().top)
-      const tooltipHeight = tooltip.clientHeight;
-      // const tooltipBottom = window.innerHeight - tooltip.getBoundingClientRect().top - tooltip.offsetHeight;
-      // const distanceBetween = tooltipBottom - target.getBoundingClientRect().top;
-      const distance = itemHeight + tooltipHeight + 20;
-
-
-      if (target.getBoundingClientRect().top < tooltipHeight + 20) {
-        target.classList.add('js-inversed');
-        tooltip.style.transform = `translateY(${distance}px)`;
-      } else {
-        target.classList.remove('js-inversed');
-        tooltip.style.transform = `translateY(0)`;
-
-      }
-
+      formulaItems.forEach(elem => elem.classList.remove('active-item'));
       target.classList.add('active-item');
     }
   });
 
-  formulaSection.addEventListener('mouseout', (e) => {
-    let target = e.target;
-
-    if (target.closest('.formula-item')) {
-      target = target.closest('.formula-item');
-
-      target.classList.remove('active-item');
-    }
-  });
 };
 
 export default tooltipHandler;
