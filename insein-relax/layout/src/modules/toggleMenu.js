4478be6e8eaca0d99f313661bab0c2436717eb96
Alex Grobovsky <ktotaam@mail.ru>
2021-05-14 16:50:06 +0500

    fix: burger menu 575p position

--- insein-relax/layout/src/modules/toggleMenu.js
+++ insein-relax/layout/src/modules/toggleMenu.js
@@ -3,29 +3,16 @@ const toggleMenu = () => {
   const menuWrapper = document.querySelector('.popup-menu'),
     menu = menuWrapper.querySelector('.popup-dialog-menu');
 
-
-
   //TODO избавиться от скачка при появлении скроллбара
-
-  // let translate3d = 'translate3d(645px, 0, 0)';
-
-  if (document.documentElement.clientWidth < 576) {
-    // menu.style.transform = 'translate3d(0, -100%, 0)';
-    // translate3d = 'translate3d(0, -100%, 0)';
-  }
 
   const openMenu = () => {
     menuWrapper.style.visibility = 'visible';
     menu.classList.add('js-opened-burger');
-    // menu.style.transform = 'translate3d(0, 0, 0)';
-    // menu.style.visibility = 'visible';
     document.body.classList.add('js-locked');
   };
 
   const closeMenu = () => {
     menuWrapper.style.visibility = 'hidden';
-    // menu.style.transform = translate3d;
-    // menu.style.visibility = 'hidden';
     menu.classList.remove('js-opened-burger');
     document.body.classList.remove('js-locked');
   };
